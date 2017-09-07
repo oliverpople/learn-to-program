4b26264f9c0951254e85780f214cb1b104342b67
oliverpople <oliverpople@gmail.com>
2017-09-07 18:34:43 +0100

    Adding excercise 10.5: Ninety-nine bottles of beer on the wall

---
+++ chap10/99_Bottles_of_Beer.rb
@@ -0,0 +1,15 @@
+def bottles(num_at_start)
+
+  num_now = num_at_start
+
+  while num_now > 0
+    puts num_now.to_s + ' bottles of beer on the wall, ' +
+         num_now.to_s + ' bottles of beer!'
+    num_now = num_now - 1
+
+    puts 'Take one down, pass it around, ' +
+          num_now.to_s + ' bottles of beer on the wall!'
+  end
+end
+
+bottles(9999)
